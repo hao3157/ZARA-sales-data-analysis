d650a39eeb24cc2224fbd66a4b454f20fe81e367
hao3157 <parker3157@gmail.com>
2025-11-05 23:37:00 +0800

    Update eda_visualization.py

--- eda_visualization.py
+++ eda_visualization.py
@@ -3,40 +3,23 @@ import matplotli
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="whitegrid")
-plt.figure(figsize=(8, 6))
-sns.histplot(df['Sales Volume'], bins=30, kde=True)
+plt.figure(figsize=(8, 6)) #圖表長寬
+sns.histplot(df['Sales Volume']) #圖表設定histogram
 plt.title('Sales Volume histogram')
 plt.tight_layout()
 plt.show()
 
 
 plt.figure(figsize=(8, 6))
-sns.histplot(df['price'], bins=30, kde=True)
+sns.histplot(df['price'])
 plt.title('Price histogram')
 plt.tight_layout()
 plt.show()
 
 
 plt.figure(figsize=(8, 6))
-sns.boxplot(x='Seasonal', y='Sales Volume', data=df)
+sns.boxplot(x='Seasonal', y='Sales Volume', data=df) #箱形圖
 plt.title('Box Plot of Sales Volume by Seasonal Category')
 plt.show()
 
-from sklearn.preprocessing import LabelEncoder
 
-cols_to_encode = ['Promotion', 'Seasonal', 'season']
-le = LabelEncoder()
-for col in cols_to_encode:
-    df[col] = le.fit_transform(df[col])
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-selected_cols = ['Promotion', 'Seasonal', 'Sales Volume', 'price', 'season']
-corr_matrix = df[selected_cols].corr()
-
-plt.figure(figsize=(10, 8))
-sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title("Correlation Heatmap of Selected Features")
-plt.tight_layout()
-plt.show()
